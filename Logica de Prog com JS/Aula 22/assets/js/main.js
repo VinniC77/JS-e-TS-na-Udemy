7924f9ac5fc22640edf06cabd3d0097becde6b2a
ViniClunc <vinniclunc@hotmail.com>
2021-10-22 00:23:49 -0300

    Corrigindo titulos das aulas

--- Logica de Prog com JS/Aula 22/assets/js/main.js
+++ Logica de Prog com JS/Aula 22/assets/js/main.js
@@ -1,69 +1,84 @@
-// Capturar evento de submit do formulário
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
 
-const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', (event)=> {
-    event.preventDefault();
-    const inputPeso = event.target.querySelector('#peso');
-    const inputAltura = event.target.querySelector('#altura');
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
+// Cria um li (do html) e retorna o mesmo, que nada mais é do que ir mostrando a lista das tarefas que estão sendo criadas:
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
 
-    if (!peso) {
-        setResultado('Peso inválido', false);
-        return;
+// Criando um evento que quando o "Enter" for pressionado, após ser digitada a tarefa, o JS o adicione à lista de tarefas.
+inputTarefa.addEventListener('keypress', (e) => {
+    // console.log(e); // O console.log() no e (evento), serve para descobrir qual é o código da tecla "Enter". O keyCode da tecla Enter é o 13.
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
     }
+})
 
-    if (!altura) {
-        setResultado('Altura inválida',false);
-        return;
-    }
+// Criando uma função para limpar o texto do input depois de ter adicionado a tarefa.
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus(); // método para o cursor voltar e piscar no campo do formulário.
+}
 
-    const imc = getImc(peso, altura);
-    const nivelImc = getNivelImc(imc);
+// Criando uma função para criar o botaõ Apagar ao lado da tarefa adicionada.
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    // botaoApagar.classList.add('apagar'); // Para manioular o botão, poderiamos atribuir uma classe à ele, mas vamos usar o setAtribute ao invés de usar o classList.
+    botaoApagar.setAttribute('class', 'apagar'); // Aqui definimos o botaoApagar como uma classe e já nomeamos a mesma como 'apagar'.
+    botaoApagar.setAttribute('title', 'Apagar essa tarefa');
+    li.appendChild(botaoApagar); // botaoApagar agora pe filha da li.
+}
 
-    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+// Insere o texto digitado pelo usuário dentro de uma li(item da lista de tarefas)
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li); // li agora é filha da classe tarefas.
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
 
-    setResultado(msg, true);
-
-    console.log(imc);
+// Pegando o valor do campo que será adicionado quando clicado no botão. E corrigindo para que quando nada for digitado, que o JS não adicione essa "tarefa". E adicionando o que for colocado ali na função de criar uma tarefa.
+btnTarefa.addEventListener('click', () => {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
 });
 
-function getNivelImc(imc) {
-    const nivel = ['Abaixo do Peso', 'Peso Normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3'];
-
-    if (imc >= 39.9) { // Não há necessidade de utilizar o else if, pois, assim não cumprindo a condição, o JS nem executa o que está nela.
-        return nivel[5];
-    } 
-    if (imc >= 34.9) return nivel[4]; 
-    if (imc >= 29.9) return nivel[3];
-    if (imc >= 24.9) return nivel[2];
-    if (imc >= 18.5) return nivel[1];
-    if (imc < 18.5) return nivel[0];
-}
-
-function getImc(peso, altura) {
-    const imc = peso / altura ** 2;
-    return imc.toFixed(2);
-}
-
-function criaP() {
-    const paragrafo = document.createElement('paragrafo');
-    return paragrafo;
-}
-
-function setResultado(msg, isValid) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = ""; // Limpando o formulario.
-
-    const paragrafo = criaP();
-
-    if (isValid) {
-        paragrafo.classList.add('paragrafo-resultado');
-    } else {
-        paragrafo.classList.add('bad');
+// Adicionando a funcionalidade de apagar ao botão.
+document.addEventListener('click', (e) => {
+    const elemento = e.target; // Para verificarmos em que parte da página o usuário clicou.
+    if (elemento.classList.contains('apagar')) { // Se esse botão contém a classe apagar, ou seja, se o usuário está clicando no botão apagar
+        elemento.parentElement.remove(); // Selecionando o parent do botão e fazendo com que ele seja apagado. Assim, tudo será apagado.
+        salvarTarefas(); // Para que seja apagado do banco de dados também, no localStorage.
     }
+})
 
-    paragrafo.innerHTML = msg;
-    resultado.appendChild(paragrafo);
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON); // Aqui é o local de onde iremos recuperar esses dados, onde só podemos salvar strings, por isso a conversão para JSON.
+    // Jogamos as tarefas para dentro de um array, pois, queremos que seja criado um JSON, que será uma string
 }
+
+// Função para ler as tarefas e jogar elas de volta para a Web, para quando atualizarmos a página, elas continuem lá.
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas); // Tornando em array novamente.
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
+}
+function adicionaTarefasSalvas();
